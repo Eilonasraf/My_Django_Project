04d0e6f50ff50f541ac78b46fc5e9679b74334d7
Eilonasraf <eilonasraf@gmail.com>
2022-04-30 21:48:21 +0300

    Added movies 2 function

--- Myproject/Movies/views.py
+++ Myproject/Movies/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Movies (request):
     return render(request, 'Movies.html')
+
+def Movies2 (request):
+    return render(request, 'home.html')
