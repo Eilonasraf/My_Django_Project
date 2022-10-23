38bd458f959315f77a92ca51351186083c7f83ee
Eilonasraf <103586426+Eilonasraf@users.noreply.github.com>
2022-10-23 13:23:59 +0300

    Update views.py

--- Myproject/Movies/views.py
+++ Myproject/Movies/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from docutils.utils.math.latex2mathml import mo
+from scipy.interpolate import interp1d
+
+from Movies.models import Movies as MoviesModel
 
 # Create your views here.
 def Movies (request):
     return render(request, 'Movies.html')
 
-def Movies2 (request):
-    return render(request, 'home.html')
+def movie_details(request, movie_id):
+    movies_obj = MoviesModel
+    get_movie = MoviesModel.objects.filter(id=movie_id)
+    return render(request, 'movie_details.html', {'movies': get_movie})
